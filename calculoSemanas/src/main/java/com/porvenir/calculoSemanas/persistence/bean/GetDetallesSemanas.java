349e2d7a9bcb6c120f226e7eec8d908a7d093065
SebastianEspitia <espitia834@gmail.com>
2023-05-12 08:58:44 -0500

    Add files via upload

---
+++ calculoSemanas/src/main/java/com/porvenir/calculoSemanas/persistence/bean/GetDetallesSemanas.java
@@ -0,0 +1,35 @@
+package com.porvenir.calculoSemanas.persistence.bean;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@Component
+@Log4j2
+public class GetDetallesSemanas {
+
+    @Resource(lookup = "jdbc/pooled/DSAPPDBPORVESRV")
+    private DataSource datasource;
+
+
+    public void generateSecuencia(Integer numeroCuenta) {
+
+        String sentence = "select * from mhistoria.semanas_historia_laboral.get_detalles_y_semanas(" + numeroCuenta + ")";
+        Connection connection = null;
+        try {
+            connection = datasource.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sentence);
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+        } catch (Exception e) {
+        }
+    }
+
+
+}
---
+++ calculoSemanas/src/test/java/com/porvenir/calculoSemanas/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porvenir.calculoSemanas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
